44a9c2fce793ad4f968e316c28a40c7b2d06d039
Hernany <contatohernany@gmail.com>
2019-07-14 02:03:57 -0300

    Iniciei o projeto, iniciei o package.json, e criei o api/server.js

---
+++ api/server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb');
+
+var app = express();
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+app.listen(port);
+
+console.log('Servidor HTTP esta escutando na porta ' + port);
