1488452c9072acbb9739cc3d20636555de273535
Hernany <contatohernany@gmail.com>
2019-07-15 03:20:58 -0300

    Criei as funcoes de POST(create), GET(ready), GET by ID (ready), PUT by ID (update), DELETE by ID (remover) da api REST

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectId;
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.get('/', function(req, res){
     res.send({msg: 'Olá'});
 });
 
+// POST (create)
 app.post('/api', function(req, res){
 
     var dados = req.body;
@@ -43,4 +45,82 @@ app.post('/api', function(req, res){
 
     });
     
+});
+
+// GET (ready)
+app.get('/api', function(req, res){
+    
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+// GET by ID (ready)
+
+app.get('/api/:id', function(req,res){
+
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find(objectId(req.params.id)).toArray(function(err, result){
+                if(err){
+                    res.json(err);
+                }else {
+                    res.json(result);
+                }
+                mongoclient.close();
+
+            });
+
+        });
+
+    });
+});
+
+
+// PUT by ID (update)
+
+app.put('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.update(
+                { _id : objectId(req.params.id) },
+                { $set : { titulo : req.body.titulo }},
+                {},
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    }else{
+                        res.json(records);
+                    }
+                    mongoclient.close();
+                }
+            );
+        });
+    });
+});
+
+// DELETE by ID (remover)
+app.delete('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.remove({ _id: objectId(req.params.id)}, function(err, records){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+
 });
