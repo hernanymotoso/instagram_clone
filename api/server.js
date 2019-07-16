60db3cb28eadd85a67ed614d1d9823bb24a5bdc7
Hernany <contatohernany@gmail.com>
2019-07-16 01:33:16 -0300

    Criei a funcionalidade de fazer uploads de fotos e exibir as fotos postadas na home

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    multiparty = require('connect-multiparty'),
     mongodb = require('mongodb'),
-    objectId = require('mongodb').ObjectId;
+    objectId = require('mongodb').ObjectId,
+    fs = require('fs');
 
 var app = express();
 
 // body-parser
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var port = 8080;
 
@@ -29,26 +32,48 @@ app.get('/', function(req, res){
 // POST (create)
 app.post('/api', function(req, res){
 
-    var dados = req.body;
-
-    db.open( function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.insert(dados, function(err, records){
-                if(err){
-                    res.json(err);
-                }else {
-                    res.json(records);
-                }
-                mongoclient.close();
-            });
-        });
-
-    });
+    res.setHeader('Access-Control-Allow-Origin', '*');
     
+    var date = new Date();
+    var time_stamp = date.getTime();
+
+    var url_imagem =  time_stamp + '_' + req.files.arquivo.originalFilename;
+     
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+
+   
+
+    fs.rename(path_origem, path_destino, function(err){
+        if(err){
+            res.status(500).json({error: err});
+            return;
+        }
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+
+        db.open( function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.json({'status' : 'erro'});
+                    }else {
+                        res.json({'status' : 'inclusao realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
+            });
+    
+        });
+    });    
 });
 
 // GET (ready)
 app.get('/api', function(req, res){
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
     
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
@@ -85,6 +110,24 @@ app.get('/api/:id', function(req,res){
     });
 });
 
+
+app.get('/uploads/:imagem', function(req, res){
+
+    var img = req.params.imagem;
+
+    fs.readFile('./uploads/'+img, function(err, content){
+        if(err){
+            res.status(400).json(err);
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-type' : 'image/jpg'
+        });
+        res.end(content);
+        
+    });
+});
 
 // PUT by ID (update)
 
